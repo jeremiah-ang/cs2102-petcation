7de849e20cb9db85964e0df01188e3019af2bc0d
Jeremiah Ang <jeremiahang@Jerms-Macbook.local>
2018-02-26 14:37:53 +0800

    initial

---
+++ htdocs/createUser.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title> Create User </title>
+	</head>
+	<body>
+		<h1> Create User </h1>
+		<form> 
+			<ul>
+				<li><label>Username: </label><input type="text" name="userId"/></li>
+				<li><label>Full Name: </label><input type="text" name="name"/><br/></li>
+				<li><label>Date Of birth: </label><input type="text" name="dateOfBirth"/><br/></li>
+				<li><label>Address: </label><textarea type="text" name="address"/></textarea><br/></li>
+			</ul>
+		</form>
+	</body>
+</html>
